051cc7620dd5612551b6705600844ec4fb765949
ProfJust <olaf.just@w-hs.de>
2025-05-02 11:53:02 +0200

    IK

--- controllers/rtde-Simulation/test_client.py
+++ controllers/rtde-Simulation/test_client.py
@@ -14,23 +14,23 @@ gripper.activate()
 gripper.set_force(50)
 gripper.set_speed(100)
 
-# Liste von kartesischen Zielposen (Dummy → direkt als joint angles interpretiert)
-moveL_ziele = [
+# Liste von Positionen (Gelenkwinkel)
+waypoints_joint_angles = [
     [-0.19, -2.10, -1.67, -2.75, 0.99, 2.99],   
     [-0.62, -1.38, -1.67, -1.81, 0.37, 2.99],     
 ]
-
-# Bewegungen ausführen
-input("Roboter startet Bewegung nach Eingabe beliebiger Taste")
-for i, ziel_pose in enumerate(moveL_ziele):
+# Bewegungen mit MoveJ ausführen
+input("Roboter startet Bewegung (MoveJ) nach Eingabe beliebiger Taste")
+for i, ziel_pose in enumerate(waypoints_joint_angles ):
     print(f"➡️  Sende moveL #{i+1}: {ziel_pose}")
-    resp = rtde_c.moveL(ziel_pose, speed=0.5, acceleration=0.3)
+    #MoveL resp = rtde_c.moveL(ziel_pose, speed=0.5, acceleration=0.3)
+    resp = rtde_c.moveJ(ziel_pose)
     print(f"📥 Antwort: {resp}")
     actual_q = rtde_r.getActualQ() # in radian
     print("Aktueller Zustand - Gelenkpositionen in Grad ")
     for arg in actual_q:
         print(arg *180.0/3.1415927)
-    time.sleep(1)
+    time.sleep(2)
 
 # Gripper Aktionen
 input("Gripper startet Bewegung nach Eingabe beliebiger Taste")
@@ -45,36 +45,3 @@ time.sleep(1)
 # Abschluss
 rtde_c.disconnect()
 rtde_r.disconnect()
-
-"""  
-# Gripper Aktionen
-gripper.activate()
-gripper.set_force(50)
-gripper.set_speed(100)
-gripper.close()
-
-# Roboter bewegen
-input("Roboter startet Bewegung nach Eingabe beliebiger Taste")
-rtde_c.moveL([-0.300, -0.700 , 0.4,  0.0, 2.204, 0.1], 0.5, 0.3)
-print("🤖 moveL...")
-# rtde_c.moveL([-0.3, -0.7, 0.4, 0.0, 3.2, 0.1])
-gripper.open()
-#gripper.move_and_wait_for_pos(255, 255, 255)
-actual_q = rtde_r.getActualQ() # in radian
-print("Aktueller Zustand - Gelenkpositionen in Grad ")
-for arg in actual_q:
-    print(arg *180.0/3.1415927)
-
-time.sleep(1)
-# Roboter bewegen
-input("Roboter startet Bewegung nach Eingabe beliebiger Taste")
-print("🤖 moveJ..")
-rtde_c.moveL([-0.600, -0.900 , 0.5,  0.0, 0.8, 0.1], 0.5, 0.3)
-gripper.close()
-time.sleep(1)
-#gripper.move(10)  # mm
-
-# Abschluss
-rtde_c.disconnect()
-rtde_r.disconnect()
-"""
