74691a0cb1c71449eb761bedc0ed9cee902de971
ProfJust <olaf.just@w-hs.de>
2025-05-23 16:57:25 +0200

    TCP no worx

---
+++ controllers/ur3_rtde_controller_camera_3d/Test vorwärts Kinematic mit DH.py
@@ -0,0 +1,64 @@
+# Test vorwärts Kinematic mit DH
+# sollte ergeben [-0.4565, 0, 0.6655]
+###### FUNKTIONIERT NICHT wie es soll
+
+#### TCP ermitteln ##############################################################
+import numpy as np
+
+# DH-Parameter für UR3e (in Metern und Radians)
+dh_params = [
+    {'a': 0,      'd': 0.1519, 'alpha': np.pi/2},
+    {'a': -0.24365, 'd': 0,      'alpha': 0},
+    {'a': -0.21325,  'd': 0,      'alpha': 0},
+    {'a': 0,      'd': 0.11235, 'alpha': np.pi/2},
+    {'a': 0,      'd': 0.08535, 'alpha': -np.pi/2},
+    {'a': 0,      'd': 0.0819, 'alpha': 0}
+]
+
+def compute_forward_kinematics(joint_angles):
+    T = np.eye(4)
+    
+    # DH-Transformationen
+    for i in range(6):
+        theta = joint_angles[i]
+        a = dh_params[i]['a']
+        d = dh_params[i]['d']
+        alpha = dh_params[i]['alpha']
+        
+        Ti = np.array([
+            [np.cos(theta), -np.sin(theta)*np.cos(alpha),  np.sin(theta)*np.sin(alpha), a*np.cos(theta)],
+            [np.sin(theta),  np.cos(theta)*np.cos(alpha), -np.cos(theta)*np.sin(alpha), a*np.sin(theta)],
+            [0,              np.sin(alpha),                np.cos(alpha),               d               ],
+            [0,              0,                            0,                           1               ]
+        ])
+        T = np.dot(T, Ti)
+    
+    # Korrekturmatrix **nach** den DH-Transformationen
+    R_correct = np.array([
+        [0, 1, 0, 0],    # X_Webots = Y_DH
+        [-1, 0, 0, 0],   # Y_Webots = -X_DH
+        [0, 0, 1, 0],    # Z bleibt gleich
+        [0, 0, 0, 1]
+    ])
+    #T = np.dot(T, R_correct)
+
+    # Nach der Koordinatensystem-Korrektur:
+    T_offset = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, -0.174],  # 174 mm in Z-Richtung
+        [0, 0, 0, 1]
+    ])  
+    T = np.dot(T, T_offset)
+    
+    return T
+# sollte ergeben [-0.4565, 0, 0.6655]
+#ergibt TCP-Position: [-0.4569  -0.02025  0.06655]
+#### TCP ermitteln ##############################################################
+
+current_joint_angles = [0, 0, 0, 0, 0, 0] # => sollte ergeben [-0.4565, 0, 0.6655]
+#                                                     ergibt  [0.4565, -0.19425, 0.06655]
+T = compute_forward_kinematics(current_joint_angles)
+
+
+print("TCP-Position:", T[:3, 3])  # Erwartet: [-0.4565, 0, 0.6655]
--- controllers/ur3_rtde_controller_camera_3d/ur3_rtde_controller_camera_3d.py
+++ controllers/ur3_rtde_controller_camera_3d/ur3_rtde_controller_camera_3d.py
@@ -52,34 +52,53 @@ import numpy as np
 # DH-Parameter für UR3e (in Metern und Radians)
 dh_params = [
     {'a': 0,      'd': 0.1519, 'alpha': np.pi/2},
-    {'a': 0.2435, 'd': 0,      'alpha': 0},
-    {'a': 0.213,  'd': 0,      'alpha': 0},
-    {'a': 0,      'd': 0.1123, 'alpha': np.pi/2},
-    {'a': 0,      'd': 0.0853, 'alpha': -np.pi/2},
-    {'a': 0,      'd': 0.0921, 'alpha': 0}
+    {'a': -0.24365, 'd': 0,      'alpha': 0},
+    {'a': -0.21325,  'd': 0,      'alpha': 0},
+    {'a': 0,      'd': 0.11235, 'alpha': np.pi/2},
+    {'a': 0,      'd': 0.08535, 'alpha': -np.pi/2},
+    {'a': 0,      'd': 0.0819, 'alpha': 0}
 ]
 
 def compute_forward_kinematics(joint_angles):
-    T = np.eye(4)  # Identitätsmatrix als Start
+    T = np.eye(4)
+    
+    # DH-Transformationen
     for i in range(6):
         theta = joint_angles[i]
         a = dh_params[i]['a']
         d = dh_params[i]['d']
         alpha = dh_params[i]['alpha']
         
-        # Einzelne Transformationsmatrix berechnen
         Ti = np.array([
             [np.cos(theta), -np.sin(theta)*np.cos(alpha),  np.sin(theta)*np.sin(alpha), a*np.cos(theta)],
             [np.sin(theta),  np.cos(theta)*np.cos(alpha), -np.cos(theta)*np.sin(alpha), a*np.sin(theta)],
             [0,              np.sin(alpha),                np.cos(alpha),               d               ],
             [0,              0,                            0,                           1               ]
         ])
-        T = np.dot(T, Ti)  # Matrizen multiplizieren
+        T = np.dot(T, Ti)
+    
+    # Korrekturmatrix **nach** den DH-Transformationen
+    R_correct = np.array([
+        [0, 1, 0, 0],    # X_Webots = Y_DH
+        [-1, 0, 0, 0],   # Y_Webots = -X_DH
+        [0, 0, 1, 0],    # Z bleibt gleich
+        [0, 0, 0, 1]
+    ])
+    T = np.dot(T, R_correct)
+
+    # Nach der Koordinatensystem-Korrektur:
+    T_offset = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, -0.174],  # 174 mm in Z-Richtung
+        [0, 0, 0, 1]
+    ])  
+    T = np.dot(T, T_offset)
+    
     return T
-
+# sollte ergeben [-0.4565, 0, 0.6655]
+#ergibt TCP-Position: [-0.4569  -0.02025  0.06655]
 #### TCP ermitteln ##############################################################
-
-
 
 # Debug: Zeige alle verfügbaren Devices
 print("🔍 Verfügbare Devices:")
@@ -238,9 +257,6 @@ while robot.step(timestep) != -1:
     distance = rf_image[y * width + x] # Zugriff auf einen einzelnen Wert
     #print("Distance ", distance)
     #range_finder.saveImage("image_rf1.jpg", 100)
-
-   
-
    
     with lock:
         # Aktuelle Gelenkpositionen aktualisieren
@@ -252,12 +268,16 @@ while robot.step(timestep) != -1:
         print(f"aktuelle Gelenkwinkel: {current_joint_angles[0]:.2f}, {current_joint_angles[1]:.2f}, {current_joint_angles[2]:.2f}, {current_joint_angles[3]:.2f}, {current_joint_angles[4]:.2f}, {current_joint_angles[5]:.2f}",     end=" ")
         #print("aktuelle Gelenkwinkel: ", current_joint_angles, end=" ")                               
                                    
-        #####  TCP ################## 
+        #####  TCP ##################     
         T = compute_forward_kinematics(current_joint_angles)
-        tcp_position = T[:3, 3]          # [x, y, z] in Metern
-        tcp_orientation = T[:3, :3]      # 3x3 Rotationsmatrix      
-       #print(" TCP: ",tcp_position )
-        print(f" TCP: {tcp_position[0]:.2f},  {tcp_position[1]:.2f}, {tcp_position[2]:.2f}")
+        print("TCP-Position:", T[:3, 3])
+        print(" TCP ###### FUNKTIONIERT NICHT wie es soll ")  
+
+        #tcp_position = T[:3, 3]          # [x, y, z] in Metern
+        # tcp_orientation = T[:3, :3]      # 3x3 Rotationsmatrix      
+        #print(" TCP: ",tcp_position )
+        #tcp_position[1] = -tcp_position[1]  # Achse umdrehen
+        #print(f"   TCP: X {tcp_position[0]:.2f}, Y {tcp_position[1]:.2f}, Z {tcp_position[2]:.2f}")
         
         # Roboterbewegung steuern
         for name, angle in zip(joint_names, target_joint_angles):
