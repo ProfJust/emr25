e283e7d30d74baf93ab9800636114186e34f0cfe
ProfJust <olaf.just@w-hs.de>
2025-06-05 10:31:33 +0200

    tips & tricks

---
+++ installation scripts/increase_swap_to_16G.sh
@@ -0,0 +1,11 @@
+# https://www.digitalocean.com/community/tutorials/how-to-add-swap-space-on-ubuntu-22-04
+free -h
+df -h
+sudo swapoff /swapfile     # alten Swap ausschalten
+sudo fallocate -l 16G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon --show
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
